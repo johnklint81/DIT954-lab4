50966a26dad8defae7e7b49a65d3076b84cba287
John Klint <gusjohn25@student.gu.se>
2025-03-03 19:44:50 +0100

    Vec2 immutable

--- Vec2.java
+++ Vec2.java
@@ -1,6 +1,6 @@
 public final class Vec2 {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
 
     static Vec2 ZERO = new Vec2(0,0);
 
@@ -9,34 +9,29 @@ public final class Vec2 {
         this.y = y;
     }
 
-    public void setPos(double x, double y) {
-        this.x = x;
-        this.y = y;
+    public Vec2 setPos(double x, double y) {
+        return new Vec2(x, y);
     }
 
-    public void setPos(Vec2 pos) {
-        x = pos.x;
-        y = pos.y;
+    public Vec2 setPos(Vec2 pos) {
+        return new Vec2(pos.x, pos.y);
     }
 
     public double distanceTo(Vec2 other) {
         return Math.sqrt(Math.pow(x - other.x, 2) + Math.pow(y - other.y, 2));
     }
 
-    public void add(double x, double y) {
-        this.x += x;
-        this.y += y;
+    public Vec2 add(double x, double y) {
+        return new Vec2(this.x + x, this.y +y);
     }
 
-
-    public void add(Vec2 other) {
-        add(other.x, other.y);
+    public Vec2 add(Vec2 other) {
+        return add(other.x, other.y);
     }
 
     public Vec2 copy() {
         return new Vec2(x, y);
     }
-
 
     public double getX() {
         return x;
